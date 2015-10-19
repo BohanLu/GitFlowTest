b5a74f56628fcad1d0c2b1463f4813dee0f287f7
Bohan <LvBohan@gengee.cn>
2015-10-19 12:16:18 +0800

    Separate list operations from main function into a function.

--- main.c
+++ main.c
@@ -5,17 +5,22 @@
 
 #define DIR_PATH    "/home/bohan/tmp/"
 
-int main(int argc, char** argv) {
+void list_folder(char *path){
 
     DIR *dir = NULL;
     struct dirent *dp;
 
-    printf("List dir [%s]...\n", DIR_PATH);
-    dir = opendir(DIR_PATH);
+    printf("List dir [%s]...\n", path);
+    dir = opendir(path);
     while ((dp = readdir(dir))){
         printf("    [%s]\n", dp->d_name);
     }
-    printf("List dir [%s] completed\n", DIR_PATH);
+    printf("List dir [%s] completed\n", path);
 
+}
+
+int main(int argc, char** argv) {
+
+    list_folder(DIR_PATH);
     return EXIT_SUCCESS;
 }
