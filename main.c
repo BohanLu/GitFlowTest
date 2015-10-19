ee3f73705b7d4b9d56448b0f62f7d76c8aa49c31
Bohan <LvBohan@gengee.cn>
2015-10-19 12:24:42 +0800

    Add a feature of counting all elements of specific folder.

--- main.c
+++ main.c
@@ -5,18 +5,40 @@
 
 #define DIR_PATH    "/home/bohan/tmp/"
 
-void list_folder(char *path){
+void list_number_of_elements(const char *path){
+
+    int counts = folder_operation(path, false);
+    printf("Number of elements in dir [%s] = %d\n", path, counts);
+    return;
+}
+
+void list_folder(const char *path){
+
+    folder_operation(path, true);
+    return;
+}
+
+int folder_operation(const char *path, bool is_print){
 
     DIR *dir = NULL;
     struct dirent *dp;
+    int counts = 0;
 
-    printf("List dir [%s]...\n", path);
+    if (is_print)
+        printf("List dir [%s]...\n", path);
+
     dir = opendir(path);
     while ((dp = readdir(dir))){
-        printf("    [%s]\n", dp->d_name);
-    }
-    printf("List dir [%s] completed\n", path);
 
+        if (is_print)
+            printf("    [%s]\n", dp->d_name);
+        counts++;
+    }
+
+    if (is_print)
+        printf("List dir [%s] completed\n", path);
+
+    return counts;
 }
 
 int main(int argc, char** argv) {
